ebb0bfc3aaf4b7064d6e2385b7058c07690405e6
Hoang Huyen <1851061774@e.tlu.edu.vn>
2022-06-28 18:38:54 +0700

    edit about immersive

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,24 @@
               $(".mean-nav").toggle();
             });
           });
+
+
+
+
+          $('.slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            autoplay:true,
+            asNavFor: '.slider-nav'
+          });
+          $('.slider-nav').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for',
+            dots: false,
+            centerMode: true,
+            focusOnSelect: true,
+            autoplay:true
+          });
